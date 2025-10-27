43fc00022ec1f768992660b4fe16d4d61bc42fc1
DragunWF <plarisanmarc2@gmail.com>
2025-10-27 19:18:39 +0800

    fix: fix initialization of sqlite3 database

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-import NavigationWrapper from "./presentation/navigation/NavigationWrapper";
+import { ActivityIndicator, View } from "react-native";
 import Toast from "react-native-toast-message";
+
+import NavigationWrapper from "./presentation/navigation/NavigationWrapper";
+import { initDatabase } from "./infrastructure/database/coreStorage";
 
 export default function App() {
   const [isDbInitialized, setIsDbInitialized] = useState(false);
 
   useEffect(() => {
     const initializeDb = async () => {
-      // Initializes the database once the app loads
-      setIsDbInitialized(true);
+      try {
+        // Initializes the database once the app loads
+        await initDatabase();
+        setIsDbInitialized(true);
+        console.log("✅ Database initialized in App.tsx");
+      } catch (error) {
+        console.error("❌ Failed to initialize database:", error);
+      }
     };
+
     initializeDb();
   }, []);
+
+  if (!isDbInitialized) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
     <>
