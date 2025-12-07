5e6580dc0f96906ce0540a75a091beacd30b0b0f
DragunWF <plarisanmarc2@gmail.com>
2025-12-07 20:18:59 +0800

    fix(app): change outdated gemini model to gemini-2.5-flash

--- app/src/infrastructure/services/geminiService.ts
+++ app/src/infrastructure/services/geminiService.ts
@@ -18,7 +18,7 @@ import {
 
 // NOTE: This public key is temporary
 const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY as string;
-const MODEL_NAME = "gemini-2.0-flash";
+const MODEL_NAME = "gemini-2.5-flash";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${MODEL_NAME}:generateContent?key=${API_KEY}`;
 
 export class GeminiService implements AIService {
